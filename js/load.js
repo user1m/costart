512e8094dfd8307ae8202c19135d2c8eb834133d
Claudius <mbembac@gmail.com>
2014-08-12 22:24:16 -0400

    inital site design

---
+++ js/load.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor:"#EEEEEE",
+        backgroundColor: "#212E3D",
+        percentage: true,
+        barHeight: 1,
+        completeAnimation: "grow",
+        minimumTime: 100
+    });
+});
